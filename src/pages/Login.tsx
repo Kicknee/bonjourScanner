6e6896d2ace5e547c533033502da93c552738d9c
Kicknee <daniszewski21@gmail.com>
2024-07-01 23:53:23 +0200

    create and style login input for Login Component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,6 +14,31 @@ const Login = () => {
             <img className="logo img-fluid" src={logo} alt="Bonjour Logo" />
           </div>
         </div>
+        <div className="row justify-content-center">
+          <div className="col-md-6 col-lg-4">
+            <form
+              action="javascript:void(0);"
+              className="d-flex justify-content-center h-75"
+            >
+              <input
+                type="text"
+                className="opacity-75 w-75 border-0 bg-input-color p-3"
+                value={input}
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
+                placeholder="Password"
+              />
+              <button className="btn">
+                <FontAwesomeIcon
+                  icon={faArrowRightToBracket}
+                  style={{ color: "#ffffff" }}
+                  className="h-100"
+                />
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
