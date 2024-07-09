2fd9d49ed2df2e143692e48b55fd442ed16e2734
Kicknee <daniszewski21@gmail.com>
2024-07-09 14:46:41 +0200

    create netlify function get_products

---
+++ netlify/functions/get_products/get_products.ts
@@ -0,0 +1,28 @@
+import { Handler } from "@netlify/functions";
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
+config();
+const client = new MongoClient(process.env.MONGODB_URI);
+
+export const handler: Handler = async () => {
+  try {
+    await client.connect();
+
+    const database = await client.db(process.env.MONGODB_DATABASE);
+    const collection = database.collection(process.env.MONGODB_COLLECTION);
+
+    const products = await collection.find({}).toArray();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    };
+  } finally {
+    await client.close();
+  }
+};
