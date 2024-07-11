10831b807561ac3b5e829ff64b59dc7266f194d2
Kicknee <daniszewski21@gmail.com>
2024-07-11 14:38:58 +0200

    create ProductQR component to display qr code for seleceted product

--- src/components/ProductDetailsTaskbar.tsx
+++ src/components/ProductDetailsTaskbar.tsx
@@ -1,31 +1,53 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilePen, faTrash } from "@fortawesome/free-solid-svg-icons";
-import qrImg from "../assets/download.png";
+import {
+  faFilePen,
+  faTrash,
+  faFloppyDisk,
+  faBan,
+} from "@fortawesome/free-solid-svg-icons";
+import ProductQR from "./ProductQR";
 
 const ProductDetailsTaskbar = ({ editMode }) => {
   return (
     <div className="row justify-content-end">
       <div className="col-7">
         <div className="row align-items-end justify-content-around">
-          <div className="col-3">
-            <button className="btn">
-              <FontAwesomeIcon
-                className="fa-3x"
-                icon={faFilePen}
-                style={{ color: "#ffffff" }}
-              />
-            </button>
-            <button className="btn">
-              <FontAwesomeIcon
-                className="fa-3x"
-                icon={faTrash}
-                style={{ color: "#ffffff" }}
-              />
-            </button>
-          </div>
-          <div className="col-9">
-            <img src={qrImg} className="img-fluid" alt="qr-code" />
-          </div>
+          {editMode ? (
+            <div className="col-3">
+              <button className="btn">
+                <FontAwesomeIcon
+                  className="fa-3x"
+                  icon={faFloppyDisk}
+                  style={{ color: "#ffffff" }}
+                />
+              </button>
+              <button className="btn">
+                <FontAwesomeIcon
+                  className="fa-3x"
+                  icon={faBan}
+                  style={{ color: "#ffffff" }}
+                />
+              </button>
+            </div>
+          ) : (
+            <div className="col-3">
+              <button className="btn">
+                <FontAwesomeIcon
+                  className="fa-3x"
+                  icon={faFilePen}
+                  style={{ color: "#ffffff" }}
+                />
+              </button>
+              <button className="btn">
+                <FontAwesomeIcon
+                  className="fa-3x"
+                  icon={faTrash}
+                  style={{ color: "#ffffff" }}
+                />
+              </button>
+            </div>
+          )}
+          <ProductQR />
         </div>
       </div>
     </div>
---
+++ src/components/ProductQR.tsx
@@ -0,0 +1,11 @@
+import qrImg from "../assets/download.png";
+
+const ProductQR = () => {
+  return (
+    <div className="col-9">
+      <img src={qrImg} className="img-fluid" alt="qr-code" />
+    </div>
+  );
+};
+
+export default ProductQR;
