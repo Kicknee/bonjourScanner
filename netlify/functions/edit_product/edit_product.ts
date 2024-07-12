07be7ab7f4f393bbbb539748853d8be68959683f
Kicknee <daniszewski21@gmail.com>
2024-07-12 20:37:45 +0200

    create netlify funtion update_product

---
+++ netlify/functions/edit_product/edit_product.ts
@@ -0,0 +1,34 @@
+import { Handler } from "@netlify/functions";
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
+config();
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+export const handler: Handler = async (event) => {
+  try {
+    await client.connect();
+
+    const database = await client.db(process.env.MONGODB_DATABASE);
+    const collection = database.collection(process.env.MONGODB_COLLECTION);
+
+    const updatedProduct = event.body ? JSON.parse(event.body) : null;
+
+    const response = await collection.updateOne(
+      updatedProduct.STYLE,
+      updatedProduct
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ response }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    };
+  } finally {
+    await client.close();
+  }
+};
