4d2060b36f108c0ac2c006082ab720f9d137d0a4
Kicknee <daniszewski21@gmail.com>
2024-07-15 22:49:24 +0200

    create a separate utils function responsible for connecting to the db

--- netlify/utils/connect_db.ts
+++ netlify/utils/connect_db.ts
@@ -4,11 +4,19 @@ config();
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
-export default async () => {
-  await client.connect();
+export default async (callback) => {
+  try {
+    await client.connect();
 
-  const database = await client.db(process.env.MONGODB_DATABASE);
-  const collection = database.collection(process.env.MONGODB_COLLECTION);
+    const database = await client.db(process.env.MONGODB_DATABASE);
+    const collection = database.collection(process.env.MONGODB_COLLECTION);
 
-  return collection;
+    const result = await callback(collection);
+
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.close();
+  }
 };
