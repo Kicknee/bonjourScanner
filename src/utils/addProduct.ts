2059e21d133f4f16a666dcacaddacb7f71dcec83
Kicknee <daniszewski21@gmail.com>
2024-07-16 21:37:05 +0200

    create utility function addProduct to connect with the add_product netlify function

---
+++ src/utils/addProduct.ts
@@ -0,0 +1,22 @@
+import { ProductType } from "../types/types";
+
+export default async (product: ProductType) => {
+  try {
+    const response = await fetch("/.netlify/functions/add_product", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
