dbf1a9063f1dcd5b46580a645c88d74b4a31b315
Kicknee <daniszewski21@gmail.com>
2024-07-17 20:27:05 +0200

    fix display ProductDetailsAddMode component, when no record is selected

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,10 +1,10 @@
-import ProductDetailsTaskbar from "./ProductDetailsTaskbar";
 import ProductDetailsEditMode from "./ProductDetailsEditMode";
 import useEditState from "../state/hooks/useEditState";
 import useProductState from "../state/hooks/useProductState";
 import ProductDetailsViewMode from "./ProductDetailsViewMode";
 import useAddState from "../state/hooks/useAddState";
 import ProductDetailsAddMode from "./ProductDetailsAddMode";
+import ProductDetailsTaskbar from "./ProductDetailsTaskbar";
 
 const Product = () => {
   const addMode = useAddState();
@@ -14,15 +14,16 @@ const Product = () => {
   return (
     <div className="row">
       <div className="col-12">
-        {editMode && !addMode && (
+        {addMode && <ProductDetailsAddMode />}
+        {selectedProduct._id && editMode && !addMode && (
           <ProductDetailsEditMode productProp={selectedProduct} />
         )}
-        {!editMode && !addMode && (
+        {selectedProduct._id && !editMode && !addMode && (
           <ProductDetailsViewMode productProp={selectedProduct} />
         )}
-        {addMode && <ProductDetailsAddMode />}
-
-        <ProductDetailsTaskbar editMode={editMode} addMode={addMode} />
+        {(selectedProduct._id || addMode) && (
+          <ProductDetailsTaskbar addMode={addMode} editMode={editMode} />
+        )}
       </div>
     </div>
   );
