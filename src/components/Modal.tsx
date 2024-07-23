a306866607864f13b4a16ac4e7f0ed5083bc13d3
Kicknee <daniszewski21@gmail.com>
2024-07-23 19:20:25 +0200

    add fade effect to modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,17 @@
 import useModalState from "../state/hooks/useModalState";
 import { showModalState } from "../state/slices/modalSlice";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Modal = () => {
   const { message } = useModalState();
   const dispatch = useDispatch();
+  const [fadeClass, setFadeClass] = useState("");
 
   useEffect(() => {
     const timer = setTimeout(() => {
       dispatch(showModalState(false));
+      setFadeClass("fade");
     }, 5000);
 
     return () => clearTimeout(timer);
@@ -17,10 +19,9 @@ const Modal = () => {
 
   return (
     <div
-      id="modal"
-      className="d-flex align-items-center justify-content-center position-absolute"
+      className={`d-flex align-items-center justify-content-center position-absolute z-3 ${fadeClass}`}
     >
-      <div className="px-5 py-4 bg-input-color  shadow rounded-2 opacity-75">
+      <div className="px-5 py-4 bg-input-color shadow rounded-2 ">
         {message}
       </div>
     </div>
