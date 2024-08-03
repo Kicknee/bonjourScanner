e98f8f48e46f6d5e469bd3d12add7bd34c91f1dc
Kicknee <daniszewski21@gmail.com>
2024-08-03 18:35:13 +0200

    fix name of DataCallback in connect_db

--- netlify/utils/connect_db.ts
+++ netlify/utils/connect_db.ts
@@ -2,7 +2,7 @@ import { Collection, MongoClient } from "mongodb";
 import { config } from "dotenv";
 config();
 
-type DatabaCallback = (collection: Collection) => Promise<object>;
+type DataCallback = (collection: Collection) => Promise<object>;
 
 const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DATABASE;
@@ -14,7 +14,7 @@ if (!uri || !dbName || !collectionName) {
 
 const client = new MongoClient(uri);
 
-export default async (callback: DatabaCallback) => {
+export default async (callback: DataCallback) => {
   try {
     await client.connect();
 
