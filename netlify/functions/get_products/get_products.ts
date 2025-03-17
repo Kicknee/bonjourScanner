b919e8f3f8bb62626e412424f4d7d430be79ef22
Kicknee <daniszewski21@gmail.com>
2025-03-17 04:34:57 +0100

    add method variable

--- netlify/functions/get_products/get_products.ts
+++ netlify/functions/get_products/get_products.ts
@@ -1,12 +1,15 @@
 import { Handler } from "@netlify/functions";
 import connect_db from "../../utils/connect_db";
+import createResponse from "../../utils/createResponse";
 
 export const handler: Handler = async () => {
+  const method = "GET";
+
   try {
     const products = await connect_db(async (collection) => {
       return await collection.find({}).toArray();
     });
-
+    // if(!products) return createResponse(404, method, "")
     return {
       statusCode: 200,
       headers: {
