f217900a25aa8fa79b61d00f7de9596131e25aaa
Kicknee <daniszewski21@gmail.com>
2025-03-28 05:30:47 +0100

    change breakpoint for container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,7 +28,7 @@ const Home = () => {
   return (
     <div className="row align-items-center" style={{ overflow: "hidden" }}>
       {showModal && <Modal />}
-      <div className="col-12 px-4 px-md-5 mx-auto col-xl-7">
+      <div className="col-12 px-4 px-md-5 mx-auto col-xxl-7">
         <div className="row d-none d-md-flex" style={{ height: "660px" }}>
           <LeftSide />
           <SectionDivider />
