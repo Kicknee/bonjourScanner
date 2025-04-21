8cbd9f0a4e400dda260cca90e5d6e457194eb4c0
Kicknee <daniszewski21@gmail.com>
2025-04-21 05:03:45 +0200

    implement createResponse in netlify get_products

--- netlify/functions/get_products/get_products.ts
+++ netlify/functions/get_products/get_products.ts
@@ -9,20 +9,34 @@ export const handler: Handler = async () => {
     const products = await connect_db(async (collection) => {
       return await collection.find({}).toArray();
     });
-    // if(!products) return createResponse(404, method, "")
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET",
-      },
-      body: JSON.stringify(products),
-    };
+    if (!products || products.length === 0)
+      return createResponse(404, method, "Couldn't get records");
+
+    return createResponse(
+      200,
+      method,
+      "Records have been downloaded",
+      products
+    );
+
+    // return {
+    //   statusCode: 200,
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Access-Control-Allow-Headers": "Content-Type",
+    //     "Access-Control-Allow-Methods": "GET",
+    //   },
+    //   body: JSON.stringify(products),
+    // };
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
+    // return {
+    //   statusCode: 500,
+    //   body: JSON.stringify(error),
+    // };
+    return createResponse(
+      500,
+      method,
+      error instanceof Error ? error.message : "Unexpected Error"
+    );
   }
 };
