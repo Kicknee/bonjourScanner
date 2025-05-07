956620e9f17c0a53f3327064d2800d18b6a96d92
Kicknee <daniszewski21@gmail.com>
2025-05-07 06:13:01 +0200

    add custom class to implement fade effect

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,28 +4,23 @@ import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 
 const Modal = () => {
-  const [fadeClass, setFadeClass] = useState("");
+  const [fadeOut, setFadeOut] = useState(false);
   const { show, message } = useModalState();
   const dispatch = useDispatch();
 
   if (!show) return;
 
   useEffect(() => {
+    setFadeOut(true);
     const timer = setTimeout(() => {
       dispatch(showModalState(false));
-      setFadeClass("fade");
     }, 3000);
-
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div
-      className={`d-flex align-items-center justify-content-center position-absolute z-3 ${fadeClass}`}
-    >
-      <div className="px-5 py-4 bg-input-color shadow rounded-2 ">
-        {message}
-      </div>
+    <div className={`custom-modal ${fadeOut ? "custom-fade" : ""}`}>
+      <div className="custom-modal-dialog">{message}</div>
     </div>
   );
 };
