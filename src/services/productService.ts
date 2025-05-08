6f0229c75b218ef44f42719b1ecbd3dc6963d5b6
Kicknee <daniszewski21@gmail.com>
2025-05-08 07:39:12 +0200

    implement apiClient

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,80 +1,18 @@
 import { ProductType } from "../types/types";
+import { apiClient } from "./apiClient";
 
 const productService = {
-  add: async (product: ProductType) => {
-    try {
-      const response = await fetch("/.netlify/functions/add_product", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product),
-      });
+  add: (product: ProductType) =>
+    apiClient("/.netlify/functions/add_product", "POST", product),
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+  delete: (product: ProductType) =>
+    apiClient("/.netlify/functions/delete_product", "DELETE", product),
 
-      const jsonResponse = await response.json();
-      return jsonResponse;
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  },
-  delete: async (product: ProductType) => {
-    try {
-      const response = await fetch("/.netlify/functions/delete_product", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product),
-      });
+  update: (product: ProductType) =>
+    apiClient("/.netlify/functions/update_product", "PATCH", product),
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const jsonResponse = await response.json();
-      return jsonResponse;
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  },
-  update: async (product: ProductType) => {
-    try {
-      const response = await fetch("/.netlify/functions/update_product", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const jsonResponse = await response.json();
-      return jsonResponse;
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  },
-  get: async () => {
-    try {
-      let response = await fetch("/.netlify/functions/get_products");
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const jsonResponse = await response.json();
-      return jsonResponse;
-    } catch (error) {
-      console.error("err", error);
-    }
-  },
+  get: () =>
+    apiClient<ProductType[]>("/.netlify/functions/get_products", "GET"),
 };
 
 export default productService;
