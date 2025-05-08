6367061f5f2461db2e45ae390a6a1a319ccd1c8a
Kicknee <daniszewski21@gmail.com>
2025-05-08 08:31:04 +0200

    remove unused actions of productListSlice

--- src/store/slices/productListSlice.ts
+++ src/store/slices/productListSlice.ts
@@ -8,23 +8,8 @@ const productListSlice = createSlice({
   initialState,
   reducers: {
     fillProductListState: (_, action) => action.payload,
-    updateProductState: (state, action) => {
-      return state.map((product) => {
-        if (product._id === action.payload._id) {
-          return action.payload;
-        } else {
-          return product;
-        }
-      });
-    },
-    deleteProductState: (state, action) => {
-      return state.filter(
-        (product) => product._id !== action.payload._id && product
-      );
-    },
   },
 });
 
 export default productListSlice.reducer;
-export const { fillProductListState, updateProductState, deleteProductState } =
-  productListSlice.actions;
+export const { fillProductListState } = productListSlice.actions;
