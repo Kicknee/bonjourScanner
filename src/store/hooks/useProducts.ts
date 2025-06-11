6f73292c7fb55c6416ed9a6439aa3dd73c03f13f
Kicknee <daniszewski21@gmail.com>
2025-06-11 05:25:57 +0200

    implement react query to handle all productService operations

---
+++ src/store/hooks/useProducts.ts
@@ -0,0 +1,43 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import productService from "../../services/productService";
+import { ProductType } from "../../types/types";
+
+// Fetch all products
+export const useProducts = () =>
+  useQuery({ queryKey: ["products"], queryFn: productService.get });
+
+// Add product
+export const useAddProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (product: ProductType) => productService.add(product),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["products"] }),
+  });
+};
+
+// Update product
+export const useUpdateProduct = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (updatedProduct: ProductType) =>
+      productService.update(updatedProduct),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
+  });
+};
+
+// Delete product
+export const useDeleteProduct = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (updatedProduct: ProductType) =>
+      productService.delete(updatedProduct),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
+  });
+};
