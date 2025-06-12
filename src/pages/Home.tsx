cc5bdd4bb30e37b572960be09a387ae73a3f064d
Kicknee <daniszewski21@gmail.com>
2025-06-12 16:01:01 +0200

    refactor code by removing logic to fetch products and move logVisitor to a separate file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,51 +2,14 @@ import LeftSide from "../components/layoutComponents/LeftSide";
 import RightSide from "../components/layoutComponents/RightSide";
 import CenterSide from "../components/layoutComponents/CenterSide";
 import SectionDivider from "../components/layoutComponents/SectionDivider";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fillProductListState } from "../store/slices/productListSlice";
 import Modal from "../components/Modal";
 import useModalState from "../store/hooks/useModalState";
-import { triggerModal } from "../utils/triggerModal";
-import productService from "../services/productService";
+import useLogVisitor from "../store/hooks/useLogVisitor";
 
 const Home = () => {
   const { show: showModal } = useModalState();
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    //get user ip and send through netlify function to google spreadsheet
-    const logVisitor = async () => {
-      try {
-        const ipRes = await fetch("https://api.ipify.org?format=json");
-        const { ip } = await ipRes.json();
-
-        await fetch("/.netlify/functions/logVisitor", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ip,
-            url: window.location.href,
-          }),
-        });
-      } catch (error) {
-        console.error("Visitor log failed: ", error);
-      }
-    };
-
-    logVisitor();
-
-    (async () => {
-      const response = await productService.get();
-      if (!response || response.status === 400 || response.status === 404) {
-        triggerModal(response.message || "Couldn't refresh product list");
-      } else {
-        dispatch(fillProductListState(response.payload));
-      }
-    })();
-  }, []);
+  useLogVisitor();
 
   return (
     <div className="row align-items-center" style={{ overflow: "hidden" }}>
