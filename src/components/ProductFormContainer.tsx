8401c617cb905040224b8978b51f821c12de686e
Kicknee <daniszewski21@gmail.com>
2025-07-15 20:21:46 +0200

    implement react hook form

--- src/components/ProductFormContainer.tsx
+++ src/components/ProductFormContainer.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent } from "react";
 import { useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFloppyDisk, faBan } from "@fortawesome/free-solid-svg-icons";
@@ -10,66 +10,136 @@ import { deselectProductState } from "../store/slices/productSlice";
 import { setMode } from "../store/slices/productModeSlice";
 import ProductQR from "./ProductQR";
 import { useAddProduct, useUpdateProduct } from "../store/hooks/useProducts";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface Props {
   mode: "add" | "edit";
   productProp?: ProductType;
 }
 
-const defaultInput: ProductType = {
-  STYLE: "",
-  TYPE: "",
-  PLACE: "",
-  LEFT: 0,
-  COLOR: "",
-  BRAND: "",
-  SHIPPING_COMPANY: "",
-};
+const ProductSchema = z.object({
+  STYLE: z
+    .string()
+    .length(5, { message: "STYLE must have 5 characters" })
+    .regex(/^[a-zA-Z0-9]+$/, {
+      message: "Field can contain only letters and numbers",
+    }),
+  TYPE: z
+    .string()
+    .nonempty({ message: "TYPE is required" })
+    .max(20, { message: "Field can have at most 20 characters" })
+    .regex(/^[a-zA-Z0-9!\/\- ]+$/, {
+      message: "Field can contain only letters and numbers",
+    }),
+  PLACE: z
+    .string()
+    .length(3, { message: "STYLE must have 5 characters" })
+    .regex(/^[a-zA-Z0-9]+$/, {
+      message: "Field can contain only letters and numbers",
+    }),
+  LEFT: z
+    .number()
+    .max(10000, { message: "LEFT value can't exceed 10000 units" }),
+  COLOR: z
+    .string()
+    .nonempty({ message: "COLOR is required" })
+    .max(10, { message: "COLOR can have at most 10 characters" })
+    .regex(/^[a-zA-Z0-9 ]+$/, {
+      message: "Field can contain only letters and numbers",
+    }),
+  BRAND: z
+    .string()
+    .nonempty({ message: "COLOR is required" })
+    .max(20, { message: "COLOR can have at most 20 characters" })
+    .regex(/^[a-zA-Z0-9!\/\- ]+$/, {
+      message: "Field can contain only letters and numbers",
+    }),
+  SHIPPING_COMPANY: z
+    .string()
+    .nonempty({ message: "COLOR is required" })
+    .max(20, { message: "COLOR can have at most 20 characters" })
+    .regex(/^[a-zA-Z0-9!\/\- ]+$/, {
+      message: "Field can contain only letters and numbers",
+    }),
+});
 
 const ProductFormContainer = ({ mode, productProp }: Props) => {
+  console.log(productProp);
   const dispatch = useDispatch();
-  const [input, setInput] = useState<ProductType>(productProp ?? defaultInput);
+  // const [input, setInput] = useState<ProductType>(productProp ?? defaultInput);
+  const inputFields: (keyof ProductType)[] = [
+    "_id",
+    "STYLE",
+    "TYPE",
+    "PLACE",
+    "LEFT",
+    "COLOR",
+    "BRAND",
+    "SHIPPING_COMPANY",
+  ];
   const addProduct = useAddProduct();
   const updateProduct = useUpdateProduct();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<ProductType>({
+    resolver: zodResolver(ProductSchema),
+    defaultValues: productProp && productProp,
+  });
 
   const mutation = mode === "add" ? addProduct : updateProduct;
 
-  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = event.target;
-    const leftNumber =
-      id === "LEFT" && Number.isInteger(Number(value)) ? Number(value) : null;
+  // const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+  //   const { id, value } = event.target;
+  //   const leftNumber =
+  //     id === "LEFT" && Number.isInteger(Number(value)) ? Number(value) : null;
 
-    if (id === "LEFT" && leftNumber! > 10000) {
-      triggerModal("Can't enter more than 10000");
-      return;
-    }
+  //   if (id === "LEFT" && leftNumber! > 10000) {
+  //     triggerModal("Can't enter more than 10000");
+  //     return;
+  //   }
 
-    setInput((prev) => ({
-      ...prev,
-      [id]: id === "LEFT" ? Math.max(0, leftNumber!) : value.toUpperCase(),
-    }));
-  };
+  //   setInput((prev) => ({
+  //     ...prev,
+  //     [id]: id === "LEFT" ? Math.max(0, leftNumber!) : value.toUpperCase(),
+  //   }));
+  // };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  // const onSubmit = async (data: ProductType) => {
+  //   // const productData = { ...input };
 
-    const productData = { ...input };
+  //   // Validate
+  //   // const allFieldsFilled = Object.entries(productData).every(
+  //   //   ([key, value]) => {
+  //   //     if (key === "_id") return true;
+  //   //     return value !== undefined && value !== null && value !== "";
+  //   //   }
+  //   // );
 
-    // Validate
-    const allFieldsFilled = Object.entries(productData).every(
-      ([key, value]) => {
-        if (key === "_id") return true;
-        return value !== undefined && value !== null && value !== "";
-      }
-    );
+  //   // if (!allFieldsFilled) {
+  //   //   triggerModal("Fill in all fields");
+  //   //   return;
+  //   // }
 
-    if (!allFieldsFilled) {
-      triggerModal("Fill in all fields");
-      return;
-    }
+  //   try {
+  //     const response: ResponseType = await mutation.mutateAsync(data);
+  //     triggerModal(response.message);
+  //     dispatch(setMode("idle"));
+  //     dispatch(deselectProductState());
+  //   } catch (error) {
+  //     triggerModal("Unexpected error occurred.");
+  //     dispatch(setMode("idle"));
+  //   }
+  // };
 
+  const onValid = async (data: ProductType) => {
+    console.log("nowe", data);
     try {
-      const response: ResponseType = await mutation.mutateAsync(productData);
+      const response: ResponseType = await mutation.mutateAsync(data);
       triggerModal(response.message);
       dispatch(setMode("idle"));
       dispatch(deselectProductState());
@@ -79,19 +149,25 @@ const ProductFormContainer = ({ mode, productProp }: Props) => {
     }
   };
 
+  const onInvalid = (formErrors: typeof errors) => {
+    const firstError = Object.values(formErrors)[0];
+    if (firstError.message) {
+      triggerModal(firstError.message);
+    }
+  };
   const handleCancel = () => {
     dispatch(setMode("view"));
   };
 
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onValid, onInvalid)}
       className="col-9 d-flex flex-column align-items-end mx-auto"
     >
       <table className="table table-dark table-borderless fs-5 product-details-table">
         <tbody>
-          {Object.entries(input).map(([key, val]) => {
-            if (key === "_id") return null;
+          {inputFields.map((key) => {
+            // if (key === "_id") return null;
             const { displayKey } = examineEntries(key);
             return (
               <tr key={key} style={{ height: "48px" }}>
@@ -99,19 +175,26 @@ const ProductFormContainer = ({ mode, productProp }: Props) => {
                 <th>
                   <input
                     id={key}
-                    name={key}
+                    type={
+                      key === "LEFT"
+                        ? "number"
+                        : key === "_id"
+                        ? "hidden"
+                        : "text"
+                    }
+                    {...register(
+                      key,
+                      key === "LEFT" ? { valueAsNumber: true } : {}
+                    )}
                     className={
                       "opacity-75 border-0 h-50 p-1 text-uppercase " +
                       (key === "STYLE" && mode === "edit"
                         ? "bg-transparent text-text-color"
                         : "bg-input-color")
                     }
-                    type={key === "LEFT" ? "number" : "text"}
                     autoCapitalize="on"
-                    value={val?.toString() ?? ""}
-                    onChange={handleInput}
                     readOnly={key === "STYLE" && mode === "edit"}
-                    maxLength={17}
+                    maxLength={20}
                     style={{ width: "150px" }}
                   />
                 </th>
